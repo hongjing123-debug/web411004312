6dd310e3d7a8dea39246aa5f404107e1ba7f9ce5
曾周宏勁 <s1100431@gm.pu.edu.tw>
2023-05-17 20:30:52 +0800

    User

---
+++ Web411004312/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web411004312.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult SingUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SingUp(string name, string account, string password)
+        {
+            return View();
+        }
+    }
+}
