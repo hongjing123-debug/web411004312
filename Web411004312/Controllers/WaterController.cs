f862bb1ba90880dbc5c8d0d9203151592f72d836
曾周宏勁 <s1100431@gm.pu.edu.tw>
2023-05-17 19:53:22 +0800

    喝水建議

--- Web411004312/Controllers/WaterController.cs
+++ Web411004312/Controllers/WaterController.cs
@@ -9,9 +9,17 @@ namespace Web411004312.Controllers
     public class WaterController : Controller
     {
         // GET: Water
-        public ActionResult Index()
+        public ActionResult Index(string name, float? weight)
         {
+            var result = "";
+            if (weight.HasValue)
+            {
+                result = (weight * 30) + "cc";
+            }
+            ViewBag.result = result;
+            ViewBag.Name = name;
             return View();
+
         }
     }
 }
